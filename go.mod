d8482842d3e1e78f9ec1b62229a5eadef4fe12cd
John Harrington <jharringtonmn@icloud.com>
2024-07-03 20:05:04 -0600

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module harr1424/GoShare
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	files, err := os.ReadDir("uploads")
+	if err != nil {
+		http.Error(w, "Error finding available files", http.StatusInternalServerError)
+		return
+	}
+
+	data := struct {
+		Files []os.DirEntry
+	}{
+		Files: files,
+	}
+
+	templates.ExecuteTemplate(w, "index.html", data)
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		file, handler, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		localFile, err := os.Create(filepath.Join("uploads", handler.Filename))
+		if err != nil {
+			http.Error(w, "Unable to create a copy of the file on the server", http.StatusInternalServerError)
+			return
+		}
+		defer localFile.Close()
+
+		if _, err := io.Copy(localFile, file); err != nil {
+			http.Error(w, "Unable to copy file to server", http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[9:])
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/new", uploadHandler)
+	http.HandleFunc("/download", fileHandler)
+
+	fmt.Println("Starting server at https://localhost:8443")
+	err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
+	if err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+	}
+}
